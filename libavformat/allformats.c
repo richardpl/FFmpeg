ee3fec9947fb720265a3305a634aea2be4862a0d
Paul B Mahol <onemda@gmail.com>
2017-02-13 15:13:09 +0100

    avformat: add DHAV demuxer
    Signed-off-by: Paul B Mahol <onemda@gmail.com>

--- libavformat/allformats.c
+++ libavformat/allformats.c
@@ -105,6 +105,7 @@ void av_register_all(void)
     REGISTER_MUXDEMUX(DAUD,             daud);
     REGISTER_DEMUXER (DCSTR,            dcstr);
     REGISTER_DEMUXER (DFA,              dfa);
+    REGISTER_DEMUXER (DHAV,             dhav);
     REGISTER_MUXDEMUX(DIRAC,            dirac);
     REGISTER_MUXDEMUX(DNXHD,            dnxhd);
     REGISTER_DEMUXER (DSF,              dsf);
