0489f45651d8ffe688fa96c140ad6245b228d4c0
Paul B Mahol <onemda@gmail.com>
2019-05-02 17:47:46 +0200

    fix adm

--- libavfilter/allfilters.c
+++ libavfilter/allfilters.c
@@ -141,6 +141,7 @@ extern AVFilter ff_asrc_sine;
 
 extern AVFilter ff_asink_anullsink;
 
+extern AVFilter ff_vf_adm;
 extern AVFilter ff_vf_alphaextract;
 extern AVFilter ff_vf_alphamerge;
 extern AVFilter ff_vf_amplify;
