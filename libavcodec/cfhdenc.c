7cc74f0083524c0e7a3f5b766852d55c107a5a87
Paul B Mahol <onemda@gmail.com>
2021-02-13 21:49:14 +0100

    avcodec/cfhdenc: add padding to each decomposition

--- libavcodec/cfhdenc.c
+++ libavcodec/cfhdenc.c
@@ -270,9 +270,9 @@ static av_cold int cfhd_encode_init(AVCodecContext *avctx)
         int width  = i ? avctx->width >> s->chroma_h_shift : avctx->width;
         int height = i ? FFALIGN(avctx->height >> s->chroma_v_shift, 8) :
                          FFALIGN(avctx->height >> s->chroma_v_shift, 8);
-        ptrdiff_t stride = FFALIGN(width / 8, 8) * 8;
+        ptrdiff_t stride = (FFALIGN(width / 8, 8) + 64) * 8;
 
-        w8 = FFALIGN(width / 8, 8);
+        w8 = FFALIGN(width / 8, 8) + 64;
         h8 = height / 8;
         w4 = w8 * 2;
         h4 = h8 * 2;
